a2c466206f6215f4fdbf376ee9dd8ca6c4ed6eb1
Piyush Sahni <piyushsahni50@gmail.com>
2026-06-22 07:44:31 +0530

    Update CloudinaryConfig.java

--- src/main/java/com/example/urbannest/config/CloudinaryConfig.java
+++ src/main/java/com/example/urbannest/config/CloudinaryConfig.java
@@ -1,21 +1,30 @@
 package com.example.urbannest.config;
 
-
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(
                 ObjectUtils.asMap(
-                        "cloud_name", ${CLOUDINARY_CLOUD_NAME},
-                        "api_key", ${CLOUDINARY_API_KEY},
-                        "api_secret", ${CLOUDINARY_API_SECRET}
+                        "cloud_name", cloudName,
+                        "api_key", apiKey,
+                        "api_secret", apiSecret
                 )
         );
     }
